b1fa2c27d791a0b2c51482d064fe08980bbb0f80
arifiani1009 <arifiani.tc09@gmail.com>
2024-10-23 10:36:03 +0700

    Pencahayaan
    Ambient, diffuse, specular light

--- 6. Pencahayaan/main.js
+++ 6. Pencahayaan/main.js
@@ -2,22 +2,21 @@ function main(){
     var canvas = document.getElementById("myCanvas");
     var gl = canvas.getContext("webgl");
 
+    quad(1, 2, 3, 0); // Kubus depan
+    quad(2, 6, 7, 3); // Kubus kanan
+    quad(3, 7, 4, 0); // Kubus atas
+    quad(4, 5, 1, 0); // Kubus kiri
+    quad(5, 4, 7, 6); // Kubus belakang
+    quad(6, 2, 1, 5); // Kubus bawah
+
     var vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
-    var colorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-
-    var indexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
-
     //normal buffer
     var normalBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cubeNormal), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(cubeNormals), gl.STATIC_DRAW);
 
     var vertexShaderCode = document.getElementById("vertexShaderCode").text;
 
@@ -27,18 +26,19 @@ function main(){
 
     var fragmentShaderCode = `
         precision mediump float;
-        varying vec3 vPosition;
-        varying vec3 vColor;
-        varying vec3 vNormal;
-        uniform vec3 uAmbientColor;
-        uniform vec3 uDiffuseColor;
-        uniform vec3 uDiffusePosition;
-        void main(){
-            vec3 lightPos = uDiffusePosition;
-            vec3 vlight = normalize(lightPos - vPosition);
-            float dotNL = max(dot(vNormal, vlight), 0.0);
-            vec3 diffuse = vColor * uDiffuseColor * dotNL;
-            vec3 ambient = vColor * uAmbientColor;
+        varying vec3 v_Position;
+        varying vec3 v_Color;
+        varying vec3 v_Normal;
+        uniform vec3 u_AmbientColor;
+        uniform vec3 u_DiffuseColor;
+        uniform vec3 u_DiffusePosition;
+        void main() {
+            // Vektor cahaya = titik sumber cahaya - titik verteks
+            vec3 lightPos = u_DiffusePosition;
+            vec3 v_light = normalize(lightPos - v_Position);
+            float dotNL = max(dot(v_Normal, v_light), 0.0);
+            vec3 diffuse = v_Color * u_DiffuseColor * dotNL;
+            vec3 ambient = v_Color * u_AmbientColor;
             gl_FragColor = vec4(ambient + diffuse, 1.0);
         }
     `;
@@ -47,81 +47,90 @@ function main(){
     gl.shaderSource(fragmentShader, fragmentShaderCode);
     gl.compileShader(fragmentShader);    
 
-    var program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-    gl.useProgram(program);
+    var shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+    gl.useProgram(shaderProgram);
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    var aPos = gl.getAttribLocation(program, "aPosition");
-    gl.vertexAttribPointer(aPos, 3, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(aPos);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    var aColor = gl.getAttribLocation(program, "aColor");
-    gl.vertexAttribPointer(aColor, 3, gl.FLOAT, false, 0, 0);
+    var aPosition = gl.getAttribLocation(shaderProgram, "a_Position");
+    var aColor = gl.getAttribLocation(shaderProgram, "a_Color");
+    var aNormal = gl.getAttribLocation(shaderProgram, "a_Normal");
+    gl.vertexAttribPointer(
+        aPosition, 
+        3, 
+        gl.FLOAT, 
+        false, 
+        9 * Float32Array.BYTES_PER_ELEMENT, 
+        0);
+    gl.vertexAttribPointer(
+        aColor, 
+        3, 
+        gl.FLOAT, 
+        false, 
+        9 * Float32Array.BYTES_PER_ELEMENT, 
+        3 * Float32Array.BYTES_PER_ELEMENT);
+    gl.vertexAttribPointer(
+        aNormal, 
+        3, 
+        gl.FLOAT, 
+        false, 
+        9 * Float32Array.BYTES_PER_ELEMENT, 
+        6 * Float32Array.BYTES_PER_ELEMENT);
+    gl.enableVertexAttribArray(aPosition);
     gl.enableVertexAttribArray(aColor);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
-    var aNormal = gl.getAttribLocation(program, "aNormal");
-    gl.vertexAttribPointer(aNormal, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(aNormal);
 
-    //ambient light
-    var uAmbientColor = gl.getUniformLocation(program, "uAmbientColor");
-    var uDiffuseColor = gl.getUniformLocation(program, 'uDiffuseColor');
-    var uDiffusePosition = gl.getUniformLocation(program, 'uDiffusePosition');
-    var uNormal = gl.getUniformLocation(program, 'uNormal');
+    gl.viewport(100, 0, canvas.height, canvas.height);
+    gl.enable(gl.DEPTH_TEST);
 
-    gl.uniform3fv(uAmbientColor, [1.0, 1.0, 1.0]);
-    gl.uniform3fv(uDiffuseColor, [0.0, 0.0, 0.0]);
-    gl.uniform3fv(uDiffusePosition, [0.0, 0.0, 0.0]);
+    var primitive = gl.TRIANGLES;
+    var offset = 0;
+    var count = 36;  // Jumlah verteks yang akan digambar
 
-    var Pmatrix = gl.getUniformLocation(program, "uProj");
-    var Vmatrix = gl.getUniformLocation(program, "uView");
-    var Mmatrix = gl.getUniformLocation(program, "uModel");
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    var model = glMatrix.mat4.create();
+    var view = glMatrix.mat4.create();
+    glMatrix.mat4.lookAt(view,
+        [0.0, 0.0, 2.0], // di mana posisi kamera (posisi)
+        [0.0, 0.0, -2.0], // ke mana kamera menghadap (vektor)
+        [0.0, 1.0, 0.0] // ke mana arah atas kamera (vektor)
+        );
+    var projection = glMatrix.mat4.create();
+    glMatrix.mat4.perspective(projection, 
+        glMatrix.glMatrix.toRadian(90), // fov dalam radian
+        1.0,  // rasio aspek
+        0.5,  // near
+        10.0  // far
+        );
+    var uModel = gl.getUniformLocation(shaderProgram, 'u_Model');
+    var uView = gl.getUniformLocation(shaderProgram, 'u_View');
+    var uProjection = gl.getUniformLocation(shaderProgram, 'u_Projection');
 
-    var projMatrix = glMatrix.mat4.create();
-    var modMatrix = glMatrix.mat4.create();
-    var viewMatrix = glMatrix.mat4.create();
+    var uAmbientColor = gl.getUniformLocation(shaderProgram, 'u_AmbientColor');
+    gl.uniform3fv(uAmbientColor, [0.1, 0.1, 0.1]);
+    var uDiffuseColor = gl.getUniformLocation(shaderProgram, 'u_DiffuseColor');
+    gl.uniform3fv(uDiffuseColor, [0.9, 0.9, 0.9]);
+    var uDiffusePosition = gl.getUniformLocation(shaderProgram, 'u_DiffusePosition');
+    gl.uniform3fv(uDiffusePosition, [0.0, 0.0, 1.0]);
+    var uNormal = gl.getUniformLocation(shaderProgram, 'u_Normal');
 
-    glMatrix.mat4.perspective(projMatrix,
-        glMatrix.glMatrix.toRadian(90), //fov dalam radian
-        1.0,    //aspek rasio
-        0.5,    //near
-        10.0    //far
-    )
-
-    glMatrix.mat4.lookAt(viewMatrix,
-        [0.0, 0.0, 2.0], //posisi kamera
-        [0.0, 0.0, -2.0], //kemana kamera menghadap
-        [0.0, 1.0, 0.0] //kemana arah atas kamera
-    );
-
-    var angle = glMatrix.glMatrix.toRadian(1);
-    function render(time){
-        if (!freeze){
-            glMatrix.mat4.rotate(modMatrix, modMatrix, angle, [1.0,1.0,1.0]);
+    var theta = glMatrix.glMatrix.toRadian(1); // 1 derajat
+    function render() {
+        if(!freeze){
+            glMatrix.mat4.rotate(model, model, theta, [1.0, 1.0, 1.0]);
         }
-
-        gl.enable(gl.DEPTH_TEST);
-        gl.depthFunc(gl.LEQUAL);
-
+        
+        gl.uniformMatrix4fv(uModel, false, model);
+        gl.uniformMatrix4fv(uView, false, view);
+        gl.uniformMatrix4fv(uProjection, false, projection);
+        var normal = glMatrix.mat3.create();
+        glMatrix.mat3.normalFromMat4(normal, model);
+        gl.uniformMatrix3fv(uNormal, false, normal);
         gl.clearColor(1.0, 1.0, 1.0, 1.0);
-        gl.clearDepth(1.0);
-
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-        gl.uniformMatrix4fv(Pmatrix, false, projMatrix);
-        gl.uniformMatrix4fv(Vmatrix, false, viewMatrix);
-        gl.uniformMatrix4fv(Mmatrix, false, modMatrix);
-    
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-        gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
-        window.requestAnimationFrame(render);
-    }
-
-    render(1);    
+        gl.drawArrays(primitive, offset, count);
+        requestAnimationFrame(render);
+      }
+      requestAnimationFrame(render);    
 }
